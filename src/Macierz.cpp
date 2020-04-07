0a093a8b9818ccf70b98fcbd5ad4b34d78a69644
Jacob0809 <62212136+Jacob0809@users.noreply.github.com>
2020-04-07 16:36:36 +0200

    drugi

--- src/Macierz.cpp
+++ src/Macierz.cpp
@@ -1,9 +1,36 @@
 #include "Macierz.hh"
+#include "Wektor.hh"
 #include <iostream>
+
 using namespace std;
 
-istream& operator >> (istream &Strm, Macierz &Mac);
+istream& operator >> (istream &Strm, Macierz &Mac)
 {
-    
+    Wektor z;
+    for(int i=0; i<ROZMIAR; i++)
+    {
+        Strm >> z;
+        Mac.setKolumna(z,i);
+    }
+    return Strm;
 }
 
+ostream& operator << (ostream &Strm, const Macierz &Mac)
+{
+    Strm<< endl;
+    for(int i=0; i<ROZMIAR; i++)
+    {
+        Strm<< Mac.getKolumna(i) <<endl;
+    }
+    return Strm;
+}
+
+double Macierz::det()
+{
+}
+
+
+void Macierz::Transponuj()
+{
+
+}
