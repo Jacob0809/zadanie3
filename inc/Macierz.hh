e7310025ad56b3730f1b79387511a90afa306c53
Jacob0809 <62212136+Jacob0809@users.noreply.github.com>
2020-04-07 15:46:01 +0200

    pierwszy

---
+++ inc/Macierz.hh
@@ -0,0 +1,46 @@
+#ifndef MACIERZ_HH
+#define MACIERZ_HH
+
+#include "rozmiar.h"
+#include <iostream>
+using namespace std;
+
+
+
+/*
+ *  Tutaj trzeba opisac klase. Jakie pojecie modeluje ta klasa
+ *  i jakie ma glowne cechy.
+ */
+class Macierz {
+  Wektor kolumny[ROZMIAR];
+  public:
+  
+  void Transponuj();
+  double det();
+
+
+  
+
+};
+
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+istream& operator >> (istream &Strm, Macierz &Mac);
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+ostream& operator << (ostream &Strm, const Macierz &Mac);
+
+
+#endif
---
+++ inc/Wektor.hh
@@ -0,0 +1,47 @@
+#ifndef WEKTOR_HH
+#define WEKTOR_HH
+
+#include "rozmiar.h"
+#include <iostream>
+
+using namespace std;
+
+
+class Wektor {
+ 
+  double skladowe[ROZMIAR]; 
+  public:
+  
+   void setSkladowa(unsigned int index, double war){
+     skladowe[index]=war;
+   }
+   double getSkladowa(unsigned int index) const{
+     return skladowe[index];
+   }
+};
+
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+istream& operator >> (istream &Strm, Wektor &Wek)
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+ostream& operator << (ostream& Strm, const Wektor &Wek)
+Wektor operator + (Wektor Skl1, Wektor Skl2)
+Wektor operator - (Wektor Skl1, Wektor Skl2)
+double operator * (Wektor Skl1, Wektor Skl2)
+Wektor operator * (Wektor Skl1, double Skl2)
+Wektor operator / (Wektor Skl1, double Skl2)
+
+#endif
